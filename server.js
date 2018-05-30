7ae31967e0ad4e86c7e80ca24432e4f84ed1e1fb
Dubravko <woose@web.de>
2018-05-30 10:56:42 +0200

    routes created

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+
 const app = express();
 
 // DB config
@@ -17,6 +21,11 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello ....");
 });
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
