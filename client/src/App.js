ae105099a92b2ecaedf3726d604669a2b605b1a8
Dubravko <woose@web.de>
2018-06-19 17:24:03 +0200

    store erstellt

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux"; // Provider ist der "Store" der alle states der verschiedenen Komponenten enthält
+import store from "./store";
 
 import logo from './logo.svg';
 import './App.css';
@@ -9,20 +11,24 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          < Navbar />
-          <Route exact path="/" component={Landing} />{/* exact hindert daran dass nicht zwei Komponenten gleichzeitig angezeigt werden */}
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            < Navbar />
+            <Route exact path="/" component={Landing} />{/* exact hindert daran dass nicht zwei Komponenten gleichzeitig angezeigt werden */}
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </ Provider>
     );
   }
 }
