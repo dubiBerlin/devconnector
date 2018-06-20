b5a8220c5d2933ed2c2d11a7a764220a8bd40152
Dubravko <woose@web.de>
2018-06-20 13:27:54 +0200

    store finished

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+        // case SET_CURRENT_USER:
+        //     return    
+    }
+}
