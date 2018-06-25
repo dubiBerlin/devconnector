65b8911957b0a1a38056459f4832dfb6e04c337d
Dubravko <woose@web.de>
2018-06-25 11:34:47 +0200

    dashboard skeletal structure component  created

---
+++ client/src/components/dashboard/dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
